c1c18702bd59ffb80fa3a484bbf33beff080b057
Efraim Trindade <118997788+Trlnity@users.noreply.github.com>
2023-11-19 13:06:49 -0300

    Add files via upload

---
+++ AppReserva/src/app/Hotel.ts
@@ -0,0 +1,8 @@
+ export class Hotel {
+    idHotel: number=0;
+    nome: string="";
+    cidade: string="";
+    rating: number=0;
+    numQuartos: number=0;
+
+ }
---
+++ AppReserva/src/app/Quarto.ts
@@ -0,0 +1,5 @@
+export class Quarto {
+   nroQuarto: number=0;
+   nroHospedes: number=0;
+   valor: number=0;
+}
